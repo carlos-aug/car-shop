ea4a94b5df69b77d7af6ede05826e24632053509
Carlos Augusto <caarlos.san@gmail.com>
2023-02-15 09:54:53 -0300

    Add domain, routes, services e controller

--- src/Domains/Car.ts
+++ src/Domains/Car.ts
@@ -1,16 +1,17 @@
 import ICar from '../Interfaces/ICar';
 
 export default class Car {
-  private id: string | undefined;
-  private model: string;
-  private year: number;
-  private color: string;
-  private status: boolean;
-  private buyValue: number;
+  protected id: string | undefined;
+  protected model: string;
+  protected year: number;
+  protected color: string;
+  protected status: boolean;
+  protected buyValue: number;
   private doorsQty: number;
   private seatsQty: number;
 
   constructor(newCar: ICar) {
+    this.id = newCar.id;
     this.model = newCar.model;
     this.year = newCar.year;
     this.color = newCar.color;
--- src/Services/carService.ts
+++ src/Services/carService.ts
@@ -1,3 +1,11 @@
-export default class CarSerivce {
-    
+import Car from '../Domains/Car';
+import ICar from '../Interfaces/ICar';
+import CarODM from '../Models/carModel';
+
+export default class CarService {
+  async createCar(car: ICar): Promise<Car> {
+    const carODM = new CarODM();
+    const newCar = await carODM.create(car);
+    return new Car(newCar);
+  }
 }
